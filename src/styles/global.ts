07eca3fd197257551345b03fbf924c95b3ac380a
Flávio Jeferson <flaviojeferson.contato@outlook.com>
2023-10-08 14:31:22 -0300

    Introduce new global styles and theme variables.

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,18 +2,38 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   * {
+    box-sizing: border-box;
+    cursor: default;
     margin: 0;
     padding: 0;
-    cursor: default;
-    box-sizing: border-box;
   }
 
-  :focus {
+  input, button {
+    background-color: ${(props) => props.theme.white};
+    border-radius: 0.25rem;
+    border: 1px solid ${(props) => props.theme['gray-200']};
     outline: none;
+    width: 100%;
+    transition: border-color ${(props) => props.theme['transition-duration']};
+    &:hover {
+      border-color: ${(props) => props.theme['gray-400']};
+    }
+    &:focus {
+      border-color: ${(props) => props.theme['gray-600']};
+    }
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    color: ${(props) => props.theme['gray-900']};
+  }
+
+  p {
+    line-height: 135%;
+    color: ${(props) => props.theme['gray-800']};
   }
 
   body, input, button, textarea, abbr {
-    font: normal 400 1rem sans-serif;
+    font: normal 400 1rem ${(props) => props.theme['font-sans']};
     text-decoration: none;
   }
 `;
--- src/themes/defaultTheme.ts
+++ src/themes/defaultTheme.ts
@@ -1,6 +1,15 @@
 const defaultTheme = {
-  white: '#ffffff',
+  'font-sans': "'Rubik', sans-serif",
+  'gray-200': '#e5e7eb',
+  'gray-400': '#9ca3af',
+  'gray-600': '#4b5563',
+  'gray-800': '#1f2937',
+  'gray-900': '#111827',
+  'indigo-500': '#6366f1',
+  'indigo-700': '#4338ca',
+  'transition-duration': '250ms',
   black: '#000000',
+  white: '#ffffff',
 };
 
 export { defaultTheme };
