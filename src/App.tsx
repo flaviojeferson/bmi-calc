e94e294266091e03fd1dd0380bd05aa8ae781ceb
Flávio Jeferson <flaviojeferson.contato@outlook.com>
2023-10-08 09:31:13 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Calcule seu IMC</h1>
+      <p>
+        O <abbr title="Índice de Massa Corporal">IMC</abbr> é uma ferramenta
+        usada para avaliar o peso ideal de um indivíduo, além de detectar casos
+        de obesidade ou desnutrição em pessoas de diferentes idades.
+      </p>
+    </>
+  );
+};
+
+export { App };
